e2245bf11f36f1cabcd8eb12e70925763e06ec0e
Brandon Suarez <brandonsuarezduran@gmail.com>
2023-07-19 20:11:40 -0400

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Brandon Suarez on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
